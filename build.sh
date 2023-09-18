597dc11c5c15f6b901c9874ebe1833c599888a67
xinmu <xinmu@xinmu.moe>
2023-09-18 22:22:29 +0800

    f1

---
+++ build.sh
@@ -0,0 +1,49 @@
+cd rocm-core
+makepkg
+makepkg -si
+
+cd ..
+cd rocm-llvm
+makepkg
+makepkg -si
+
+cd ..
+cd rocm-roct-thunk-interface
+makepkg
+makepkg -si
+
+cd ..
+cd rocm-cmake
+makepkg
+makepkg -si
+
+cd ..
+cd rocm-device-libs
+makepkg
+makepkg -si
+
+cd ..
+cd rocm-rocr-runtime
+makepkg
+makepkg -si
+
+cd ..
+cd rocminfo
+makepkg
+makepkg -si
+
+cd ..
+cd rocm-compilersupport
+makepkg
+makepkg -si
+
+cd ..
+cd rocm-clr
+makepkg
+makepkg -si
+
+cd ..
+cd rocFFT
+makepkg
+makepkg -si
+
