076ac934c28cb7a80f84a343ad2688a66d83504c
xinmu <xinmu@xinmu.moe>
2023-10-07 16:45:06 +0800

    more build

--- build.sh
+++ build.sh
@@ -43,7 +43,61 @@ makepkg -f
 makepkg -i
 
 cd ..
-cd rocFFT
+cd rocm-rocFFT
 makepkg -f
 makepkg -i
 
+# FIXME error
+#cd ..
+#cd rocm-rocBLAS
+#makepkg -f
+#makepkg -i
+
+cd ..
+cd rocm-rocPRIM
+makepkg -f
+makepkg -i
+
+cd ..
+cd rocm-rocRAND
+makepkg -f
+makepkg -i
+
+cd ..
+cd rocm-HIPIFY
+makepkg -f
+makepkg -i
+
+
+cd ..
+cd rocm-rocSPARSE
+makepkg -f
+makepkg -i
+
+cd ..
+cd rocm_smi_lib
+makepkg -f
+makepkg -i
+
+# FIXME error
+#cd ..
+#cd rocm-rccl
+#makepkg -f
+#makepkg -i
+# error
+#cd ..
+#cd rocm-hipSPARSE
+#makepkg -f
+#makepkg -i
+
+# error `rocm-rocBLAS` `rocm-rccl` `rocm-hipSPARSE`
+# error: unknown type name 'uint64_t'
+# error: unknown type name 'uint32_t'
+# error: no member named 'int_fast8_t' in the global namespace
+# error: no member named 'int_fast16_t' in the global namespace; did you mean '__int_least16_t'?
+# error: no member named 'int_fast32_t' in the global namespace; did you mean '__int_least32_t'?
+# error: no member named 'int_fast64_t' in the global namespace; did you mean '__int_least64_t'?
+# error: no member named 'int_least8_t' in the global namespace; did you mean '__int_least8_t'?
+# error: no member named 'int_least16_t' in the global namespace; did you mean '__int_least16_t'?
+# error: no member named 'int_least32_t' in the global namespace; did you mean '__int_least32_t'?
+# error: no member named 'int_least64_t' in the global namespace; did you mean '__int_least64_t'
