f3e08d630fb7151f22e54b8c8cf12bb99413d876
xinmu <xinmu@xinmu.moe>
2025-04-04 21:15:28 +0800

    6.3.3a1

--- stage6/llama.cpp/build.sh
+++ stage6/llama.cpp/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export pkgver=6.3.2
+export pkgver=6.3.3
 export ROCM_HOME=/opt/rocm-$pkgver/
 export ROCM_PATH=$ROCM_HOME
-function fetch(){
-  git clone --recursive https://github.com/ggerganov/llama.cpp
-}
 export PATH=$ROCM_HOME/bin:$ROCM_HOME/lib/llvm/bin:$PATH
+function fetch(){
+  git clone --recursive https://github.com/ggml-org/llama.cpp
+}
 function prepare() {
   mkdir build
   cd build
@@ -14,8 +14,12 @@ function prepare() {
   else
     targetSet=( )
   fi
-  EXT_CFLAGS="-pipe -fopenmp -Wl,-rpath=/opt/rocm-${pkgver}/lib -Wl,-rpath=/opt/rocm-${pkgver}/lib/llvm/lib -fPIC -stdlib=libc++ -Wno-gnu-line-marker -L/opt/rocm-${pkgver}/lib/llvm/lib -I/opt/rocm-${pkgver}/lib/llvm/include/c++/v1 -Wno-unused-command-line-argument -I/opt/rocm-${pkgver}/include  -L/opt/rocm-${pkgver}/lib -fexperimental-library -mcmodel=extreme"
-
+  EXT_CFLAGS="-pipe -fopenmp -Wl,-rpath=/opt/rocm-${pkgver}/lib -Wl,-rpath=/opt/rocm-${pkgver}/lib/llvm/lib -fPIC -stdlib=libc++ -Wno-gnu-line-marker -L/opt/rocm-${pkgver}/lib/llvm/lib -I/opt/rocm-${pkgver}/lib/llvm/include/c++/v1 -Wno-unused-command-line-argument -I/opt/rocm-${pkgver}/include  -L/opt/rocm-${pkgver}/lib -fexperimental-library"
+  _ARCH=$(/opt/rocm-$pkgver/lib/llvm/bin/clang $EXT_FLAGS --version | grep Target|awk '{print $2}'|awk -F - '{print $1}')
+  if [ $_ARCH == 'loongarch64' ];
+  then
+    EXT_CFLAGS="-mcmodel=extreme $EXT_CFLAGS"
+  fi
   cmake ../llama.cpp \
    -DCMAKE_C_COMPILER=/opt/rocm-$pkgver/lib/llvm/bin/clang \
    -DCMAKE_CXX_COMPILER=/opt/rocm-$pkgver/lib/llvm/bin/amdclang++ \
@@ -24,7 +28,7 @@ function prepare() {
    -DCMAKE_C_FLAGS="$EXT_CFLAGS" \
    -DCMAKE_HIP_FLAGS="$EXT_CFLAGS" \
    -DGGML_HIP=ON \
-   -DCMAKE_INSTALL_RPATH="/opt/rocm-${pkgver}/lib;/opt/rocm-${pkgver}/lib/llvm/lib;/opt/rocm-${pkgver}/lib64" \
+   -DCMAKE_INSTALL_RPATH="/opt/rocm-${pkgver}/lib;/opt/rocm-${pkgver}/lib/llvm/lib;/opt/rocm-${pkgver}/lib64;.;../lib64" \
    -DCMAKE_BUILD_RPATH="/opt/rocm-${pkgver}/lib;/opt/rocm-${pkgver}/lib/llvm/lib;/opt/rocm-${pkgver}/lib64" \
    ${targetSet[@]} \
    -G "Ninja"
@@ -36,13 +40,15 @@ function build() {
   cd ..
 }
 function package(){
-  echo 
+  cd build
+    cmake --install . --prefix=$PWD/../pkg/
+  cd ..
 }
 
 function main(){
   fetch
   prepare
   build
-#  package
+  package
 }
 main
