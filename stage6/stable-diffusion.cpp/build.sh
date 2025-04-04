413a68bcb957a5609b7026682251cf4af631c650
xinmu <xinmu@xinmu.moe>
2025-04-04 21:15:28 +0800

    6.3.3a1

--- stage6/stable-diffusion.cpp/build.sh
+++ stage6/stable-diffusion.cpp/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export pkgver=6.3.2
+export pkgver=6.3.3
 export ROCM_HOME=/opt/rocm-$pkgver/
 export ROCM_PATH=$ROCM_HOME
 export PATH=$ROCM_HOME/bin:$ROCM_HOME/lib/llvm/bin:$PATH
@@ -14,7 +14,12 @@ function prepare() {
   else
     targetSet=( )
   fi
-  EXT_CFLAGS="-pipe -fopenmp -Wl,-rpath=/opt/rocm-${pkgver}/lib -Wl,-rpath=/opt/rocm-${pkgver}/lib/llvm/lib -fPIC -stdlib=libc++ -Wno-gnu-line-marker -L/opt/rocm-${pkgver}/lib/llvm/lib -I/opt/rocm-${pkgver}/lib/llvm/include/c++/v1 -Wno-unused-command-line-argument -I/opt/rocm-${pkgver}/include  -L/opt/rocm-${pkgver}/lib -fexperimental-library -mcmodel=extreme"
+  EXT_CFLAGS="-pipe -fopenmp -Wl,-rpath=/opt/rocm-${pkgver}/lib -Wl,-rpath=/opt/rocm-${pkgver}/lib/llvm/lib -fPIC -stdlib=libc++ -Wno-gnu-line-marker -L/opt/rocm-${pkgver}/lib/llvm/lib -I/opt/rocm-${pkgver}/lib/llvm/include/c++/v1 -Wno-unused-command-line-argument -I/opt/rocm-${pkgver}/include  -L/opt/rocm-${pkgver}/lib -fexperimental-library"
+  _ARCH=$(/opt/rocm-$pkgver/lib/llvm/bin/clang $EXT_FLAGS --version | grep Target|awk '{print $2}'|awk -F - '{print $1}')
+  if [ $_ARCH == 'loongarch64' ];
+  then
+    EXT_CFLAGS="-mcmodel=extreme $EXT_CFLAGS"
+  fi
   cmake ../stable-diffusion.cpp \
    -DCMAKE_C_COMPILER=/opt/rocm-$pkgver/lib/llvm/bin/clang \
    -DCMAKE_CXX_COMPILER=/opt/rocm-$pkgver/lib/llvm/bin/amdclang++ \
@@ -24,7 +29,7 @@ function prepare() {
    -DCMAKE_HIP_FLAGS="${EXT_CFLAGS}" \
    -DGGML_HIP=ON \
    -DSD_HIPBLAS=ON \
-   -DCMAKE_INSTALL_RPATH="/opt/rocm-${pkgver}/lib;/opt/rocm-${pkgver}/lib/llvm/lib;/opt/rocm-${pkgver}/lib64" \
+   -DCMAKE_INSTALL_RPATH="/opt/rocm-${pkgver}/lib;/opt/rocm-${pkgver}/lib/llvm/lib;/opt/rocm-${pkgver}/lib64;.;../lib64" \
    -DCMAKE_BUILD_RPATH="/opt/rocm-${pkgver}/lib;/opt/rocm-${pkgver}/lib/llvm/lib;/opt/rocm-${pkgver}/lib64" \
    ${targetSet[@]} \
    -G "Ninja"
@@ -36,7 +41,9 @@ function build() {
   cd ..
 }
 function package(){
-  echo 
+  cd build
+    cmake --install . --prefix=$PWD/../pkg/
+  cd ..
 }
 
 function main(){
